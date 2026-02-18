6c3e334cb61983dac57675cef36ff5ffbe23cec4
yassine007C <104086968+yassine007C@users.noreply.github.com>
2026-02-18 22:43:56 +0000

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
+import { registerRoutes } from "./routes.js";
 import { createServer } from "http";
 
 import { createRoutes } from "./routes.js";
