c0086bf6341ebe167fc973657ad8856a83b78dca
yassine007C <104086968+yassine007C@users.noreply.github.com>
2026-02-18 23:06:10 +0000

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -85,7 +85,7 @@ app.use((req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     serveStatic(app);
   } else {
-    const { setupVite } = await import("./vite");
+    const { setupVite } = await import("./vite.js");
     await setupVite(httpServer, app);
   }
 
