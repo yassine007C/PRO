3c00a820aad98fc6078738694cd61ee2f125ce3c
yassine007C <104086968+yassine007C@users.noreply.github.com>
2026-02-19 13:27:32 +0000

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,13 +1,13 @@
-import express, { type Express } from "express";
-import fs from "fs";
-import path from "path";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export function serveStatic(app: Express) {
-  import { fileURLToPath } from 'url';
-  import { dirname } from 'path';
-  
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
+
   const distPath = path.resolve(__dirname, "public");
 
   if (!fs.existsSync(distPath)) {
